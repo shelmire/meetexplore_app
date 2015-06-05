330646375233506a586e02d8948caa9475f82ea0
Alex Shelmire <shelmire@gmail.com>
2015-06-05 14:25:38 -0400

    devise basics

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, :path => '', :path_names => { :sign_in => "login", :sign_out => "logout", :sign_up => "signup" }
   root             'static_pages#home'
   get 'about'   => 'static_pages#about'
   get 'contact' => 'static_pages#contact'
