9a70a2b48c0b5f90d74b906b4277be698ac0da06
Alex Shelmire <shelmire@gmail.com>
2015-06-05 12:45:51 -0400

    email settings

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -15,7 +15,9 @@ Rails.application.configure do
   # Configure static file server for tests with Cache-Control for performance.
   config.serve_static_files   = true
   config.static_cache_control = 'public, max-age=3600'
-
+  
+  config.action_mailer.delivery_method = :test
+  config.action_mailer.default_url_options = { host: 'localhost:3000' }
   # Show full error reports and disable caching.
   config.consider_all_requests_local       = true
   config.action_controller.perform_caching = false
