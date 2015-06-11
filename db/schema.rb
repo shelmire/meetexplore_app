e6cbaf540d1d0b3e635ef826252e7d44428463a3
Alex Shelmire <shelmire@gmail.com>
2015-06-11 09:36:34 -0400

    oh shit moment

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150606154101) do
+ActiveRecord::Schema.define(version: 20150611132258) do
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
