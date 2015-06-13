370036ae58b67a58a9b3930b5fdfc651e542b671
Alex Shelmire <shelmire@gmail.com>
2015-06-13 16:49:20 -0400

    rollback changes

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,7 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+  	@user = User.new(display_name: "Example User", email: "user@example.com", password:"foobar", password_confirmation: "foobar")
+  end
 end
